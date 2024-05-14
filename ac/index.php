80569ebc13489be39c91eb69df69f7792eeb37d9
Vinicius Dias <carlosv775@gmail.com>
2024-05-14 16:09:57 -0300

    Adicionando chamada ao banco com Framework X

--- ac/index.php
+++ ac/index.php
@@ -2,11 +2,22 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Clue\React\SQLite\Factory;
 use React\Http\Message\Response;
+use function React\Async\await;
+
+$factory = new Factory();
+$db = $factory->openLazy(':memory:');
 
 $app = new FrameworkX\App();
 
 $app->get('/', fn () => Response::plaintext("Hello world!\n"));
+
+$app->get('/db', function () use ($db) {
+    $resultado = await($db->query('SELECT random() as numero;'));
+
+    return Response::plaintext("{$resultado->rows[0]['numero']}");
+});
 
 $app->run();
 
